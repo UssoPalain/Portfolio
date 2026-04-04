554cb055aac39fdc0d482bb82d3cf4ddd2ffa3fe
UssoPalain <bejaouinassim64@gmail.com>
2026-04-04 15:03:43 +0200

    vc

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -3,31 +3,29 @@ import { NavLink } from 'react-router-dom'
 
 function Projets() {
 
-    async function getProjet(){
-        try {
-            const response = await fetch(`https://api.github.com/users/UssoPalain/repos?per_page=30&sort=updated`)
-            console.log(response)
-            if (!response.ok) {
-                alert('Projets introuvable')
-            }
-            const data = await response.json()
-            console.log(data)
-            const projet = {
-                name: data.name,
-                desc: data.description,
-                lang: data.language,
-                link: data.html_url,
-                date: data.updated_at,
-                star: data.stargazers_count,
-            }
-            console.log(projet)
-            projets.set(projet)
-            return projet
-            } catch (error) {
-            console.error("Erreur lors de la récupération des projets");
-            return null;
-        }
-    }
+    async function getProjet() {
+  try {
+    const response = await fetch(
+      'https://api.github.com/users/UssoPalain/repos?per_page=30&sort=updated'
+    )
+
+    const data = await response.json()
+
+    const projetsFormates = data.map(repo => ({
+      name: repo.name,
+      desc: repo.description,
+      lang: repo.language,
+      link: repo.html_url,
+      date: repo.updated_at,
+      star: repo.stargazers_count,
+    }))
+
+    setProjets(projetsFormates)
+
+  } catch (error) {
+    console.error("Erreur lors de la récupération des projets")
+  }
+}
 
     getProjet()
     return (
