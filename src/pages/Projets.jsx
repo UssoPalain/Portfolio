d10aaaa5e182ba059e5702e155ea2c7c37c98f6a
UssoPalain <bejaouinassim64@gmail.com>
2026-04-09 13:09:54 +0200

    oui

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -2,65 +2,70 @@ import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
 function Projets() {
-    const [projets] = useState([])
-    async function getProjet() {
-  try {
-    const response = await fetch(
-      'https://api.github.com/users/UssoPalain/repos?per_page=30&sort=updated'
-    )
 
-    const data = await response.json()
+  const [projets, setProjets] = useState([])
 
-    const projets = data.map(repo => ({
-      name: repo.name,
-      desc: repo.description,
-      lang: repo.language,
-      link: repo.html_url,
-      date: repo.updated_at,
-      star: repo.stargazers_count,
-    }))
+  async function getProjet() {
+    try {
 
-    console.log(projets)
-     setProjets(projets)
-    return projets
+      const response = await fetch(
+        'https://api.github.com/users/UssoPalain/repos?per_page=30&sort=updated'
+      )
 
-  } catch (error) {
-    console.error("Erreur lors de la récupération des projets")
-    return null
+      const data = await response.json()
+
+      const projetsFormates = data.map(repo => ({
+        name: repo.name,
+        desc: repo.description,
+        lang: repo.language,
+        link: repo.html_url,
+        date: repo.updated_at,
+        star: repo.stargazers_count,
+      }))
+
+      console.log(projetsFormates)
+
+      setProjets(projetsFormates)
+
+    } catch (error) {
+      console.error("Erreur lors de la récupération des projets")
+    }
   }
-}
 
-    useEffect(() => {
-        getProjet()
-    }, [])
-    return (
-        <div>
-            <p>Projets</p>
-            <NavLink to="/">page home<br/></NavLink>
-            <NavLink to="/projects">page Projet<br/></NavLink>
-            <NavLink to="/about">page About<br/></NavLink>
-        
-        <h1>Mes projets</h1>
+  useEffect(() => {
+    getProjet()
+  }, [])
 
-        {projets.map((projet, index) => (
-            <div
-            key={index}
-            onClick={() => window.open(projet.link, "_blank")}
-            style={{
-                border: "1px solid black",
-                margin: "10px",
-                padding: "10px",
-                cursor: "pointer"
-            }}
-            >
-            <h3>{projet.name}</h3>
-            <p>{projet.desc}</p>
-            <p>{projet.lang}</p>
-            <p>⭐ {projet.star}</p>
-            </div>
-        ))}
+  return (
+    <div>
+      <p>Projets</p>
+
+      <NavLink to="/">page home<br/></NavLink>
+      <NavLink to="/projects">page Projet<br/></NavLink>
+      <NavLink to="/about">page About<br/></NavLink>
+
+      <h1>Mes projets</h1>
+
+      {projets.map((projet, index) => (
+        <div
+          key={index}
+          onClick={() => window.open(projet.link, "_blank")}
+          style={{
+            border: "1px solid black",
+            margin: "10px",
+            padding: "10px",
+            cursor: "pointer"
+          }}
+        >
+          <h3>{projet.name}</h3>
+          <p>{projet.desc}</p>
+          <p>{projet.lang}</p>
+          <p>⭐ {projet.star}</p>
         </div>
-        
-    )
+      ))}
+
+    </div>
+  )
 }
+
 export default Projets
