f61a98130493954a84cb1e72c705e69bfc6c17a3
UssoPalain <bejaouinassim64@gmail.com>
2026-04-26 18:37:42 +0200

    onRessaye

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,15 +15,15 @@ function Home() {
       </div>
 
       <img href="/130-Bejaoui Nassim-2.jpg"></img>
-
-
-
-
-
-
-
-
-
+      <p>bejaouinassim64@gmail.com</p>
+      <h3>Hello, I'm</h3>
+      <h1>Nassim Bejaoui</h1>
+      <h2>Etudiant en M2I</h2>
+      <h2>Mon Profil</h2>
+      <p>Bonjour,Je suis Nassim Béjaoui Élève de M2i. 
+        Mes Passions sont la Programmation et le domaine de L'informatique en général. 
+        Dynamique et ayant un excellent sens du contact, j'aime collaborer avec des interlocuteurs variés. 
+        Volontaire, je me distingue par mon engagement au service du collectif et mon goût d'apprendre.</p>
     </div>
   )
 }
